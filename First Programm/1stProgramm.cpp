6bf480a29b139e9628415b524b2940de352a555f
Usama Nazir <usamanazir.it@gmail.com>
2025-07-29 18:10:53 +0500

    Solve Tasks

--- First Programm/1stProgramm.cpp
+++ First Programm/1stProgramm.cpp
@@ -6,5 +6,7 @@ int main(void) {
 	cout << "Welcome to in my first programm codespace.\n"; // OR use endl instead of \n
 	cout << "Welcome to in my first programm codespace.\n";
 	cout << "Welcome to in my first programm codespace.\n";
+	
 	return 0;
 }
+
